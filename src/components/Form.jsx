a6433511b8e4f1f5766a840e9daf74012121119b
hamidr-source <hr454837@gmail.com>
2025-07-10 20:00:42 +0330

    Add data fetching

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import { fetchMovie } from "./store/MovieStore";
 
 const Form = () => {
+  const handleChangeValue = (e) => {
+    fetchMovie(e.target.value);
+  };
+
   return (
     <div>
-        <input type='text' />
+      <input type="text" onChange={handleChangeValue} id="value" />
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
--- src/components/store/MovieStore.jsx
+++ src/components/store/MovieStore.jsx
@@ -1,5 +1,27 @@
 import { create } from "zustand";
 
+const apiKey = process.env.REACT_APP_API_KEY;
+const baseUrl = process.env.REACT_APP_BASE_URL;
+
+export const fetchMovie = async (searchItem) => {
+  const url = `${baseUrl}?apikey=${apiKey}&s=${searchItem}`;
+
+  if (!searchItem) {
+    return;
+  }
+
+  try {
+    const response = await fetch(url)
+    if (!response.ok) {
+      throw new Error("Network response was not ok")  
+    }
+    const data = response.json()
+    console.log(data)
+  } catch (error) {
+    throw new Error(error.massage)  
+  }
+};
+
 const useMovieSearch = create((set) => ({
   searchItem: "",
   movies: [],
@@ -7,7 +29,6 @@ const useMovieSearch = create((set) => ({
   error: null,
 
   setSearchItem: (term) => set({ searchItem: term }),
-  
 }));
 
 export default useMovieSearch;
